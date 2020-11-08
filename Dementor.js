b22c2e501ff236295b83bfb49489ddf786fd1e2a
Queeneedu <73861561+Queeneedu@users.noreply.github.com>
2020-11-08 21:11:28 +0900

    디멘터 모양 베지어로 변경

--- Dementor.js
+++ Dementor.js
@@ -5,7 +5,7 @@ class Dementor {
     this.vel.mult(2);
     this.acc = createVector(0,0);
     this.mass = m;
-    this.r = sqrt(this.mass);
+    this.r = 2*this.mass;
   }
 
   applyForce(force) {
@@ -20,11 +20,16 @@ class Dementor {
   }
 
   show() {
+    // PerlinTest.update();
+    noStroke();
     fill(220,220,255,2);
-    ellipse(this.pos.x,this.pos.y,this.r*15);
-  
-    fill(0, 150);
-    ellipse(this.pos.x, this.pos.y, this.r * 2);
-    
+    ellipse(this.pos.x,this.pos.y,this.r);
+    stroke(0);
+    strokeWeight(3);
+    beginShape();
+    vertex(this.pos.x, this.pos.y);
+    bezierVertex(this.pos.x,this.pos.y+120, this.pos.x+60, this.pos.y+120,this.pos.x+60,this.pos.y);
+    endShape();
+    // bezier(this.pos.x,this.pos.y,this.pos.x,this.pos.y+this.r,this.pos.x+this.m,this.pos.y+this.r,this.pos.x+this.m,this.pos.y);
   }
 }
--- Patronus.js
+++ Patronus.js
@@ -1,18 +1,18 @@
 class Patronus{
   constructor(x,y,m){
     this.pos = createVector(x,y,m);
-    this.vel = createVector(0,0);
+    this.vel = createVector(1,0);
     this.acc = createVector(0,0);
  }
-  
+
   appear(){
       fill(255,255,255);
       ellipse(this.pos.x,this.pos.y,m);
     }
-  
+
   run(){
     this.vel.add(this.acc);
     this.pos.add(this.vel);
-    this.acc.set(0, 0); 
+    this.acc.set(0, 0);
   }
 }

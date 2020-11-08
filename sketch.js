01055d73f57527cbb64f34453cedcb76c3d1fcdf
Queeneedu <73861561+Queeneedu@users.noreply.github.com>
2020-11-08 21:09:59 +0900

    전체 사이즈 변경

--- sketch.js
+++ sketch.js
@@ -3,14 +3,14 @@ let player;
 let patronus;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1920, 1080);
   for (let i = 0; i < 10; i++) {
     let x = random(width);
     let y = random(height);
-    let m = random(100, 300);
+    let m = random(70, 400);
     dementors[i] = new Dementor(x, y, m);
   }
-  player = new Player(width / 2, height / 2, 100);
+  player = new Player(100);
   patronus = new Patronus(width/2,height/2,50)
   background(0);
 }
@@ -23,7 +23,7 @@ function draw() {
     dementor.show();
     player.attract(dementor);
   }
-  
+
   if (mouseIsPressed){
   patronus.run();
   patronus.appear();

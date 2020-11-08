e329a5132bbf8d2eab5d1cd040d0773e71ed3b85
Queeneedu <73861561+Queeneedu@users.noreply.github.com>
2020-11-08 21:10:25 +0900

    player pos = mouseX,Y

--- Player.js
+++ Player.js
@@ -1,24 +1,24 @@
 class Player {
-  
-  constructor(x,y,m) {
-    this.pos = createVector(x,y);
+
+  constructor(m) {
     this.mass = m;
     this.r = sqrt(this.mass)*2;
   }
-  
+
   attract(dementor) {
+    this.pos = createVector(mouseX,mouseY);
     let force = p5.Vector.sub(this.pos, dementor.pos);
-    let distanceSq = constrain(force.magSq(), 0, 100);
-    let G = 0.1;
+    let distanceSq = constrain(force.magSq(), 0, 50);
+    let G = 0.05;
     let strength = G * (this.mass * dementor.mass) / distanceSq;
     force.setMag(strength);
     dementor.applyForce(force);
   }
-  
-  
+
+
   show() {
     noStroke();
     fill(255,0, 100);
-    ellipse(this.pos.x, this.pos.y, this.r*2);    
+    ellipse(mouseX, mouseY, this.r*2);
   }
 }
